f98cd9173031288b8c609047bdad2ddf49152e58
Ivan Chen <tachen2@cisco.com>
2022-02-16 10:16:00 +0800

    Tailwind issue when using class binding

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+const switches = [{
+  on: true,
+}, {
+  on: false,
+}]
+</script>
+
+<template>
+  <div class="mt-32 space-y-16">
+    <div class="flex items-center justify-around">
+      <div
+        v-for="s of switches"
+        class="w-32 h-32 border-2 border-dashed"
+        :class="{ 'bg-pink-300': s.on, 'bg-cyan-300': !s.on }"
+      ></div>
+    </div>
+
+    <div class="flex items-center justify-around">
+      <div
+        v-for="s of switches"
+        class="w-32 h-32 border-2 border-dashed"
+        :class="{ 'bg-pink-300': s.on, 'bg-pink-300': !s.on }"
+      ></div>
+    </div>
+  </div>
+</template>
